612b9202fcbec427e18a05a7a34490115fca6418
zkyto2311 <zkyto2311@outlook.com>
2023-03-04 23:05:54 +0900

    Add theme switch button

--- js/script.js
+++ js/script.js
@@ -1,3 +1,21 @@
+'use strict';
+
+const switcher = document.querySelector('.switch');
+
+switcher.addEventListener('click', function() {
+    document.body.classList.toggle('light-theme');
+    document.body.classList.toggle('dark-theme');
+
+    const className = document.body.className;
+    if(className == "light-theme") {
+        this.textContent = "Dark Theme";
+    } else {
+        this.textContent = "Light Theme";
+    }
+
+    console.log('current class name: ' + className);
+});
+
 const result = document.querySelector(".result");  
 function calculate() {
     result.value = new Function("return " + result.value)();
